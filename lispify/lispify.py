a2a1ee641c349178c1b0501bee591b72a19d7ddb
Michael Silver <michaelssilver@me.com>
2016-07-24 16:06:34 -0400

    Copy lispify.py and test_lispify.py from wikipediabase
    For previous commit history, see WikipediaBase at commit
    cf697dddc1a071ae0dd9a8484254a3295d32d1c0
    
    On the web:
    https://github.com/infolab-csail/WikipediaBase/tree/cf697dddc1a071ae0dd9a8484254a3295d32d1c0
    
    In the shell:
    
       $ git clone git@github.com:infolab-csail/WikipediaBase.git
       $ cd WikipediaBase
       $ git checkout cf697dddc1a071ae0dd9a8484254a3295d32d1c0

---
+++ lispify/lispify.py
@@ -0,0 +1,350 @@
+"""
+Lispify converts Python objects into Lisp-like encoded strings that are
+readable by START.
+
+Use the 'lispify' function to encode a Python object.
+To create a new way of interpreting data subclass the LispType class.
+
+Prefix your class with an underscore (_) if it is an intermediate representation
+so that the lispify method ignores it.
+"""
+
+from numbers import Number
+import warnings
+
+from wikipediabase.log import Logging
+from wikipediabase.util import subclasses, camel_case_to_lisp_name
+
+
+# For fully deterministic lisp types use this priority.
+MIN_PRIORITY = 0
+MAX_PRIORITY = 15
+
+
+def mid_priority():
+    """
+    Just count. Later classes override previous ones.
+    """
+
+    for i in xrange(MIN_PRIORITY + 1, MAX_PRIORITY):
+        yield i
+
+    while True:
+        warnings.warn("Increase MAX_PRIORITY")
+        yield i
+
+MID_PRIORITY = mid_priority()
+
+
+class LispType(Logging):
+
+    """
+    A LispType is a Lisp-like encoded string that is readable by START.
+    Its `val` attribute is a Python object.
+
+    Note that not only answers but also questions are lispified.
+
+    To use this subclass it and provide any of the following:
+
+    - val_str: string representation
+    - should_parse
+    - parse_val
+    """
+
+    priority = 0
+    literal = False
+
+    def __init__(self, val):
+        """
+        Lispify a piece of data. Throws ValueError on failure.
+        """
+
+        self.val = val
+
+        if self.should_parse():
+            self.val = self.parse_val(val)
+        else:
+            raise ValueError("failed to lispify %s" % str(self.val))
+
+    def should_parse(self):
+        """
+        If this returns False, LispType is invalid whatever the value.
+        """
+
+        return True
+
+    def parse_val(self, val):
+        """
+        Override this to have special value manipulation.
+        """
+
+        return val
+
+    def __repr__(self):
+        r = u"<%s object %s>" % (self.__class__.__name__,
+                                 self.__str__())
+        return r.encode('utf-8')
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __unicode__(self):
+        return u"%s" % self.val_str()
+
+    def val_str(self):
+        return unicode(self.val)
+
+    def __nonzero__(self):
+        return True
+
+    def __eq__(self, other):
+        # compare LispType objects based on their string representation
+        if isinstance(other, self.__class__):
+            return self.__str__() == other.__str__()
+        elif isinstance(other, basestring):
+            return self.__str__() == other
+
+        return False
+
+    def __hash__(self):
+        return hash(self.__str__())
+
+
+class LispString(LispType):
+    priority = next(MID_PRIORITY)
+
+    def should_parse(self):
+        return isinstance(self.val, basestring)
+
+    def val_str(self):
+        v = self.val.replace('"', '\\"')  # escape double quotes
+        v = u'"{0}"'.format(v)
+        return v
+
+
+class LispList(LispType):
+
+    """
+    This is coordinates and other things like that
+    """
+
+    priority = next(MID_PRIORITY)
+    literal = True
+
+    def should_parse(self):
+        return hasattr(self.val, '__iter__')
+
+    def erepr(self, v):
+        if isinstance(v, LispType):
+            return unicode(v)
+
+        try:
+            return unicode(lispify(v))
+        except NotImplementedError:
+            return repr(v)
+
+    def val_str(self):
+        return u"(%s)" % (" ".join([self.erepr(v) for v in self.val]))
+
+    def __contains__(self, val):
+        return (self.erepr(val) in map(self.erepr, self.val))
+
+
+class LispDict(LispType):
+
+    """
+    Get a lispy dictionary of non-None items.
+    """
+
+    priority = next(MID_PRIORITY)
+
+    def should_parse(self):
+        return isinstance(self.val, dict)
+
+    def val_str(self):
+        pairs = sorted(self.val.items())
+        return u'(%s)' % self._plist(pairs)
+
+    def _plist(self, pairs):
+        """
+        A lispy plist without the parens.
+        """
+
+        return " ".join(list(self._paren_content_iter(pairs)))
+
+    def _kv_pair(self, k, v):
+        if k is None:
+            return u"%s" % v
+
+        if isinstance(k, basestring):
+            return u":%s %s" % (k, v)
+
+    def _paren_content_iter(self, pairs):
+        for k, v in pairs:
+            if v is not None:
+                # XXX: NastyHack(TM). Replace the nonbreaking space
+                # with a space.
+                yield self._kv_pair(k, lispify(v))
+
+
+class LispDate(LispDict):
+
+    """
+    Special LispDict that indicates a date.
+    Note: YOU are responsible for parsing the date into the format that
+    you want (e.g. yyyymmdd) using overlay framework or whatever.
+    """
+
+    priority = next(MID_PRIORITY)
+
+    # dictionary keys that indicate date format should be treated differently
+    keywords = {"yyyymmdd"}
+
+    def should_parse(self):
+        if isinstance(self.val, dict):
+            if any([(kw in self.val) for kw in self.keywords]):
+                return True
+
+    def _paren_content_iter(self, pairs):
+        for k, v in pairs:
+            if v is not None:
+                if k in self.keywords:
+                    yield self._kv_pair(k, v)
+                else:
+                    yield self._kv_pair(k, lispify(v))
+
+
+class LispError(LispType):
+
+    """
+    An error with a symbol. The expected value should be an exception
+    with the class name as desired symbol name and the following attributes:
+    :message  for Python exceptions
+    :reply    for error messages that can be displayed to START users
+              (e.g. death-date for a person that is still alive)
+    """
+
+    priority = MAX_PRIORITY
+
+    def should_parse(self):
+        return isinstance(self.val, Exception)
+
+    def val_str(self):
+        symbol = camel_case_to_lisp_name(type(self.val).__name__)
+        kw = self.val.__dict__
+        if self.val.message:
+            kw.update({'message': self.val.message})
+
+        if kw:
+            return u"(:error {symbol} {keys})".format(
+                symbol=symbol,
+                keys=self._plist(kw.items())
+            )
+        else:
+            return u"(:error %s)" % symbol
+
+    def _plist(self, pairs):
+        return " ".join(list(self._paren_content_iter(pairs)))
+
+    def _kv_pair(self, k, v):
+        if k is None:
+            return u"%s" % v
+
+        if isinstance(k, basestring):
+            return u":%s %s" % (k, v)
+
+    def _paren_content_iter(self, pairs):
+        for k, v in pairs:
+            if v is not None:
+                yield self._kv_pair(k, lispify(v))
+
+    def __nonzero__(self):
+        """
+        Errors should count as zeros so that you can check for an answer with
+
+        if potential_error_or_none:
+            # do stuff
+        """
+
+        return False
+
+
+class _LispLiteral(LispType):
+
+    """
+    Lisp literals. These are not.
+    """
+    priority = MAX_PRIORITY
+    literal = True
+
+
+class LispKeyword(_LispLiteral):
+
+    """
+    Just a keyword. No content.
+    """
+
+    def should_parse(self):
+        return isinstance(self.val, basestring) and self.val.startswith(':') \
+            and ' ' not in self.val
+
+    def val_str(self):
+        return self.val
+
+
+class LispBool(_LispLiteral):
+
+    """
+    Lispify a boolean value
+    """
+
+    def should_parse(self):
+        return isinstance(self.val, bool)
+
+    def val_str(self):
+        return u't' if self.val else u'nil'
+
+
+class LispNone(_LispLiteral):
+
+    """
+    Lispified none object
+    """
+
+    def should_parse(self):
+        return self.val is None
+
+    def val_str(self):
+        return u'nil'
+
+
+class LispNumber(_LispLiteral):
+
+    def should_parse(self):
+        return isinstance(self.val, Number)
+
+    def val_str(self):
+        return str(self.val)
+
+
+WIKIBASE_LISP_TYPES = subclasses(LispType, instantiate=False)
+
+
+def lispify(obj):
+    """
+    Return a Lisp-like encoded string.
+    """
+
+    if isinstance(obj, LispType):
+        return obj
+
+    for T in WIKIBASE_LISP_TYPES:
+        try:
+            return T(obj)
+        except ValueError:
+            pass
+
+    raise NotImplementedError(
+        "Implement LispType for val: %s or provide fallback error." % obj)
+
+__all__ = ['lispify']
---
+++ tests/test_lispify.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_lispify
+----------------------------------
+
+Tests for `lispify` module.
+"""
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from wikipediabase.lispify import lispify
+
+
+class TestLispify(unittest.TestCase):
+
+    def test_string(self):
+        self.assertEqual(lispify("foo"), '"foo"')
+
+    def test_string_escaped(self):
+        self.assertEqual(lispify('foo \\ "bar"'), '"foo \\ \\"bar\\""')
+
+    def test_encodings(self):
+        self.assertEqual(str(lispify(u"föø ” ")), u'"föø ” "'.encode('utf-8'))
+        self.assertEqual(unicode(lispify(u"föø ” ")), u'"föø ” "')
+
+    def test_list(self):
+        l = ['wikipedia-class1', 'wikipedia-class2']
+        self.assertEqual(lispify(l), '("wikipedia-class1" "wikipedia-class2")')
+
+    def test_nested_list(self):
+        l = [[0, 'foo'], [1, '"bar"']]
+        self.assertEqual(lispify(l), '((0 "foo") (1 "\\"bar\\""))')
+
+    def test_nested_tuple(self):
+        # this test needs to be separated from nested list because tuples
+        # are used in string formatting (see #222 for an example of failure)
+        t = ('foo', ('bar', 'baz'))
+        self.assertEqual(lispify(t), '("foo" ("bar" "baz"))')
+
+    def test_double_nested_list(self):
+        l = [[0, ['v0', 'foo']], [1, ['v1', 'bar']]]
+        self.assertEqual(lispify(l), '((0 ("v0" "foo")) (1 ("v1" "bar")))')
+
+    def test_list_of_dict(self):
+        l = [{'foo': 'bar'}, {'foo': 'baz'}]
+        self.assertEqual(lispify(l), '((:foo "bar") (:foo "baz"))')
+
+    def test_list_contains(self):
+        ll = lispify(["1", "3"])
+        self.assertIn("1", ll)
+        self.assertNotIn(1, ll)
+
+    def test_date_simple(self):
+        ed = lispify({'yyyymmdd': '00000808'})
+        self.assertEqual(ed, '(:yyyymmdd 00000808)')
+
+    def test_date_multiple_keys(self):
+        ed = lispify({'yyyymmdd': '19491001', 'html': 'Oct 1, 1949'})
+        self.assertEqual(ed, '(:html "Oct 1, 1949" :yyyymmdd 19491001)')
+
+    def test_bool(self):
+        self.assertEqual(lispify(True), 't')
+        self.assertEqual(lispify(False), 'nil')
+
+    def test_keyword(self):
+        self.assertEqual(lispify(':feminine'), ":feminine")
+
+    def test_string_not_keyword(self):
+        self.assertEqual(lispify(':not a keyword'), '":not a keyword"')
+
+    def test_dict(self):
+        self.assertEqual(lispify({'a': 1, 'b': "foo"}),
+                         '(:a 1 :b "foo")')
+
+    def test_dict_with_escaped_string(self):
+        self.assertEqual(lispify({'a': 1, 'b': '"foo"'}),
+                         '(:a 1 :b "\\"foo\\"")')
+
+    def test_dict_with_list(self):
+        self.assertEqual(lispify({'a': 1, 'b': ['foo', 'bar']}),
+                         '(:a 1 :b ("foo" "bar"))')
+
+    def test_error_from_exception(self):
+        err = lispify(ValueError('Wrong thing'))
+        self.assertEqual(err, '(:error value-error :message "Wrong thing")')
+
+    def test_none(self):
+        self.assertEqual(lispify(None), 'nil')
+
+    def test_number(self):
+        self.assertEqual(lispify(5), '5')
+
+    def test_lispified(self):
+        val = {"hello": "world"}
+        self.assertEqual(lispify(val), lispify(lispify(val)))
+
+    @unittest.expectedFailure
+    def test_unimplemented(self):
+        lispify(lambda x: x)
+
+if __name__ == '__main__':
+    unittest.main()
